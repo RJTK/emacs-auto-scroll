ef2d7d9328f56c1eb173123882c9fcea603ac021
Ryan J. Kinnear <Ryan@Kinnear.ca>
2018-09-16 17:22:45 -0500

    Added config variables and methods to modify scroll speed.

--- auto-scroll.el
+++ auto-scroll.el
@@ -30,46 +30,94 @@
 ;;; Code:
 
 (defgroup auto-scroll nil
-  "Let cursor scroll on a timer."
+  "Set cursor to scroll itself on a timer."
   :group 'convenience
   :prefix "auto-scroll")
 
 (define-minor-mode auto-scroll-mode
   "Minor mode for automatically scrolling the cursor"
   :init-value nil
-  :keymap '(("\C-c \M-a s" . 'auto-scroll-start))
+  :keymap '(("\C-c \M-a s" . 'auto-scroll-start)
+	    ("\C-c \M-a k" . 'auto-scroll-stop)
+	    )
+  :group 'convenience
   )
-
-(make-sparse-keymap)
 
 (defcustom auto-scroll-mode-hook nil
   "Hooks to run after command `auto-scroll-mode' is toggled."
   :group 'auto-scroll
   :type 'hook)
 
-(defvar auto-scroll-period nil)
+(defcustom auto-scroll-default-scroll-period 1
+  "Default scrolling period."
+  :group 'auto-scroll
+  :type 'number)
+
+(defcustom auto-scroll-period-granularity 0.1
+  "Granularity for increasing / decreasing scroll period."
+  :group 'auto-scroll
+  :type 'number)
+
+(defcustom auto-scroll-minimum-period 0.1
+  "Shortest allowed scrolling period."
+  :group 'auto-scroll
+  :type 'number)
+
+(defvar auto-scroll-period auto-scroll-default-scroll-period)
 (defvar auto-scroll-timer nil)
+
+(defun auto-scroll-start (&optional period)
+  "Start auto scrolling with the given PERIOD.
+
+This function can be safely called multiple times without
+starting multiple times, it will cancel the previous timer and
+start a new one with the given period."
+  (progn
+    (if period (setq auto-scroll-period period))
+    (if (timerp auto-scroll-timer)
+	(auto-scroll-stop))
+    (setq auto-scroll-timer
+	  (run-at-time auto-scroll-period auto-scroll-period
+		       'auto-scroll-next-line)))
+  )
 
 (defun auto-scroll-next-line ()
   "Move cursor to next line, if not at end of buffer.
 Otherwise, stop the auto-scroll timer"
   (if (not (eq (point) (point-max)))
       (forward-line)
-    (auto-scroll-stop)))
-
-(defun auto-scroll-start (period)
-  "Start auto scrolling with the given PERIOD."
-  (setq auto-scroll-period period)
-  (setq auto-scroll-timer
-	(run-at-time 0 period 'auto-scroll-next-line))
+    (auto-scroll-stop))
   )
 
 (defun auto-scroll-stop ()
   "Stop the auto-scroll timer."
   (if (timerp auto-scroll-timer)
-      (cancel-timer auto-scroll-timer)
-    )
+      ;; Don't try to stop a timer that hasn't been start.
+      (progn (cancel-timer auto-scroll-timer)
+	     (setq auto-scroll-timer nil))
+    ))
+
+
+(defun auto-scroll-faster ()
+  "Reduce period by period-granularity."
+  (auto-scroll-delta-period (* -1 auto-scroll-period-granularity))
   )
+
+(defun auto-scroll-slower ()
+  "Increase period by period-granularity."
+  (auto-scroll-delta-period auto-scroll-period-granularity)
+  )
+  
+(defun auto-scroll-delta-period (delta)
+  "Change period by DELTA, subject to auto-scroll-minimum-period."
+  (let ((new-period
+	 (max auto-scroll-minimum-period (+ auto-scroll-period delta))
+	 ))
+    (if (timerp auto-scroll-timer)
+      (auto-scroll-start new-period)
+      (setq auto-scroll-period new-period)
+      )
+    new-period))
 
 (provide 'auto-scroll)
 ;;; auto-scroll.el ends here
